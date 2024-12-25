372772ea2651c80c842ebe9220925548e8570b0a
xamidovscode <xamidovcoder@gmail.com>
2024-12-25 16:59:01 +0500

    new news

--- core/settings/base.py
+++ core/settings/base.py
@@ -14,16 +14,18 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
+config = dotenv_values(".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config['DEBUG']
 
 ALLOWED_HOSTS = ["*"]
 
@@ -74,10 +76,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+    "ENGINE": config['ENGINE'],
+    "NAME": config['NAME'],
+    "USER": config['USER'],
+    "PASSWORD": config['PASSWORD'],
+    "HOST": config['HOST'],
+    "PORT": config['PORT'],
+}
 }
 
 
